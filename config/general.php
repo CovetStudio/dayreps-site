84abc2f4c64715a284979e534196ffd8cbf393c8
Paul Allan <paulallan@Pauls-MBP.attlocal.net>
2019-05-06 16:50:56 -0700

    hell

--- config/general.php
+++ config/general.php
@@ -18,7 +18,7 @@ return [
         'omitScriptNameInUrls' => true,
 
         // Control Panel trigger word
-        'cpTrigger' => 'cp-dash',
+        'cpTrigger' => 'admin',
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
